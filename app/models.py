7c25614e269a5f091fae43ae31ba679b2567cd7a
BBYBOI <zeygermanleo@gmail.com>
2026-09-10 22:19:07 +0500

    add relationship

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+class Base(DeclarativeBase):
+    pass
+
+
+class AuthorOrm(Base):
+    __tablename__ = 'authors'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+
+    books: Mapped[list['BookOrm']] = relationship(back_populates='author')
+
+
+class BookOrm(Base):
+    __tablename__ = 'books'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    note: Mapped[str]
+
+    author_id: Mapped[int] = mapped_column(ForeignKey('authors.id'))
+    author: Mapped['AuthorOrm'] = relationship(back_populates='books')
