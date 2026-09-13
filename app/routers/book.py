cf88ff3d42982baf69eee0b857583c03e951cfe3
BBYBOI <zeygermanleo@gmail.com>
2026-09-13 16:01:17 +0500

    add book

--- app/routers/book.py
+++ app/routers/book.py
@@ -1,14 +1,29 @@
 from fastapi import APIRouter
+from sqlalchemy import select
 from app.database import SessionDep
-from app.schemas import CreateBook
+from app.schemas import CreateBook, ResponseBook
 from app.models import BookOrm, AuthorOrm
 
 router = APIRouter(prefix='/book', tags=['Книги'])
 
-@router.post('/', summary='Добавить книгу')
+@router.post('/', summary='Добавить книгу', response_model= ResponseBook)
 async def create_book(session: SessionDep, book: CreateBook ):
-    new_author = AuthorOrm(name = book.author.name)
-    if new_author.name not in AuthorOrm:
-        session.add(new_author)
-        await session.commit()
-    new_book = BookOrm(title = book.title, note = book.note, author = new_author)
+    query = select(AuthorOrm).where(AuthorOrm.name == book.author.name)
+    result = await session.execute(query)
+    author = result.scalar_one_or_none()
+
+    if author is None:
+        author = AuthorOrm(
+            name = book.author.name
+        )
+        session.add(author)
+
+    new_book = BookOrm(
+        title = book.title,
+        note = book.note,
+        author = author
+    )
+    session.add(new_book)
+    await session.commit()
+    await session.refresh(new_book)
+    return new_book
