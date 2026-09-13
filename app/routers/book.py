78db0ec92d08a27e663b0e8866e898e24f6f504e
BBYBOI <zeygermanleo@gmail.com>
2026-09-13 16:14:24 +0500

    get books

--- app/routers/book.py
+++ app/routers/book.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from sqlalchemy import select
 from app.database import SessionDep
-from app.schemas import CreateBook, ResponseBook
+from app.schemas import CreateBook, ResponseBook, ResponseAuthorWithBooks
 from app.models import BookOrm, AuthorOrm
 
 router = APIRouter(prefix='/book', tags=['Книги'])
@@ -27,3 +27,27 @@ async def create_book(session: SessionDep, book: CreateBook ):
     await session.commit()
     await session.refresh(new_book)
     return new_book
+
+
+@router.get('/all', summary='Показать все книги', response_model = list[ResponseBook])
+async def get_all(session: SessionDep):
+    query = select(BookOrm)
+    result = await session.execute(query)
+    books = result.scalars().all()
+    return books
+
+
+@router.get('/{book_id}', summary='Найти книгу по id', response_model= ResponseBook)
+async def get_book_by_id(session: SessionDep, book_id: int):
+    query = select(BookOrm).where(BookOrm.id == book_id)
+    result = await session.execute(query)
+    book = result.scalar_one_or_none()
+
+    if not book:
+        raise HTTPException(status_code=404, detail='Книга не найдена!')
+    return book
+
+
+@router.get('/{author_name}', summary='Показать все книги автора', response_model=ResponseAuthorWithBooks)
+async def get_author_with_books(session: SessionDep, author_name: str):
+    pass
